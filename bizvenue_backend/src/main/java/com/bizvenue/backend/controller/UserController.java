2a185261dd9d8f0166dfe936af11f516ad0551b5
ChilkawarPravin <chilkawarpravin@gmail.com>
2026-02-08 01:14:33 +0530

    user controller

---
+++ bizvenue_backend/src/main/java/com/bizvenue/backend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.bizvenue.backend.controller;
+
+import com.bizvenue.backend.dto.UserDTO;
+import com.bizvenue.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
